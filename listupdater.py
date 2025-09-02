668e1ed8d0f10fca8745332c79f50e88cda51345
j <80975779+ShadowOcto@users.noreply.github.com>
2025-09-02 18:19:58 +0100

    Added listupdater script
    Updates old ClassiDash list format to new api format for testing

---
+++ listupdater.py
@@ -0,0 +1,56 @@
+# ClassiDash Demonlist Updater
+# https://github.com/ClassiDash/Tools
+# classidash.com
+
+import sys, json
+
+ARGUMENTS = sys.argv
+
+if len(ARGUMENTS) < 3:
+    exit("Usage: listupdater input.json output.json")
+
+INPUT_PATH = ARGUMENTS[1]
+OUTPUT_PATH = ARGUMENTS[2]
+
+if not INPUT_PATH.endswith(".json"):
+    json_correct = input("Your input file doesn't begin with .json, are you sure this is correct?")
+    if not json_correct.lower() != "y" and json_correct.lower() != "yes":
+        exit("Cancelled.")
+
+OUTPUT_FORMAT = {
+    "ID": "",
+    "Name": "name",
+    "IngameID": "_id",
+    "Creator": "creators",
+    "VerifierName": "verifier",
+    "VerifierID": "",
+    "VideoURL": "video"
+}
+
+input_list = json.load(
+    open(INPUT_PATH, "r")
+)
+
+output_list = []
+
+for i, input_level in enumerate(input_list):
+    level = {}
+
+    for k, v in OUTPUT_FORMAT.items():
+        if k == "ID":
+            level[k] = i
+            continue
+        
+        if v.startswith("_"):
+            level[k] = int(input_level.get(v[1:], 0))
+            continue
+
+        level[k] = input_level.get(v, "")
+
+    output_list.append(level)
+
+json.dump(
+    output_list, 
+    open(OUTPUT_PATH, "w+"),
+    indent=2
+)
